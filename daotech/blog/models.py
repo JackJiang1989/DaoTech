a7a7bb966414f843a33af4f50ec2b411a9cb8343
Jiang Jack <CNJIANGJA@tetrapak.com>
2022-02-10 22:20:03 +0100

    test urls

--- daotech/blog/models.py
+++ daotech/blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.forms import CharField
 
 # Create your models here.
+
+class Article(models.Model):
+    author = CharField(max_length=10)
+    content = CharField(max_length=100)
