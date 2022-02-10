61f3e7e8469a310af80180734d9ab8909528e756
Jiang Jack <CNJIANGJA@tetrapak.com>
2022-02-10 22:36:31 +0100

    model

--- daotech/blog/models.py
+++ daotech/blog/models.py
@@ -1,8 +1,10 @@
+from xmlrpc.client import DateTime
 from django.db import models
-from django.forms import CharField
+from django.forms import CharField, DateField, DateTimeField
 
 # Create your models here.
 
 class Article(models.Model):
     author = CharField(max_length=10)
     content = CharField(max_length=100)
+    date = DateTimeField()
