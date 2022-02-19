a55047bdc9711061225a238722d8b9ac9ee22092
Jiang Jack <CNJIANGJA@tetrapak.com>
2022-02-19 22:31:01 +0100

    update models

--- daotech/blog/models.py
+++ daotech/blog/models.py
@@ -1,10 +1,51 @@
+from distutils.text_file import TextFile
 from xmlrpc.client import DateTime
 from django.db import models
-from django.forms import CharField, DateField, DateTimeField
+# from django.forms import CharField, DateField, DateTimeField-
 
 # Create your models here.
 
+class Tag(models.Model):
+    tag = models.CharField(max_length = 20)
+
+class Author(models.Model):
+    author = models.CharField(max_length = 20)
+
+
 class Article(models.Model):
-    author = CharField(max_length=10)
-    content = CharField(max_length=100)
-    date = DateTimeField()
+    # TAG=(
+    #     ('heat transfer','ht'),
+    #     ('mechine learning', '')
+    #     ('cooking', 'co'),
+    #     ('cycling', 'cy'),
+    #     ('reading', 're')
+    # )
+
+    author = models.ForeignKey(Author, on_delete=models.PROTECT)
+    content = models.TextField()
+    date = models.DateTimeField()
+    tag = models.ManyToManyField(Tag)
+    views = models.PositiveIntegerField(default=0)
+
+
+
+class comments(models.Model):
+    author = models.CharField(max_length = 20)
+    email = models.EmailField()
+    content = models.TextField()
+    date = models.DateTimeField()
+    
+    article = models.ForeignKey(
+        Article,
+        # verbose_name='文章',
+        on_delete=models.CASCADE) 
+        #Cascade deletes. Django emulates the behavior of the SQL constraint ON DELETE CASCADE and also deletes the object containing the ForeignKey.
+
+    parent_comment = models.ForeignKey(
+        'self',
+        # verbose_name="上级评论",
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE)            
+
+
